46b93dc1c0989460244dd1df6f4aa6fce69a03d3
hmchen1107 <langing8420@gmail.com>
2021-01-25 22:50:39 +0800

    The calculate version

--- read.py
+++ read.py
@@ -1,15 +1,21 @@
 #read
-n = 0
-
 data = []
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
-		n = n + 1
-		if n % 1000 == 0:
-			print(len(data))
+print('File has been read, there are', len(data), 'datas')	
 
-print('File has been read, there are', len(data), 'datas')
+#1000000筆字串
+#每一筆字串代表一個留言
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('每筆留言的平均長度為 : ' ,sum_len/len(data))
+
+
+
+
 
 
  
