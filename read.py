adc62f53d1131f4237f87cb77cc57dbae40e3f7d
hmchen1107 <langing8420@gmail.com>
2021-01-29 02:00:05 +0800

    Add filtering good file

--- read.py
+++ read.py
@@ -18,9 +18,19 @@ print('File has been read, there are', len(data), 'datas.')
 #==========================================
 
 
-#============清單塞選==============
-new = []
+#=================清單篩選==================
+#new = []
+#for d in data:
+        #if len(d) < 100:
+                #new.append(d)
+#print('一共有',len(new), '筆留言長度小於100')
+#==========================================
+
+
+#=================good篩選==================
+good = []
 for d in data:
-        if len(d) < 100:
-                new.append(d)
-print('一共有',len(new), '筆留言長度小於100')
+        if "good" in d:
+                good.append(d)
+print('一共有',len(good), '筆留言提到good')
+print(good[1])
