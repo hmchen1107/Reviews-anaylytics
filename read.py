af65f3a0a158aa5fc2c20331505789600bc11582
hmchen1107 <langing8420@gmail.com>
2021-01-29 01:50:14 +0800

    Add filtering file

--- read.py
+++ read.py
@@ -2,20 +2,25 @@
 data = []
 with open('reviews.txt', 'r') as f:
 	for line in f:
-		data.append(line)
-print('File has been read, there are', len(data), 'datas')	
+	    data.append(line.strip())
+print('File has been read, there are', len(data), 'datas.')	
 
 #1000000筆字串
 #每一筆字串代表一個留言
 
-sum_len = 0
+#============算留言的平均長度==============
+#sum_len = 0
+#for d in data:
+#	sum_len = sum_len + len(d)
+#print('每筆留言的平均長度為 : ' ,sum_len/len(data))
+#print(sum_len)
+#print(print(len(d))
+#==========================================
+
+
+#============清單塞選==============
+new = []
 for d in data:
-	sum_len = sum_len + len(d)
-print('每筆留言的平均長度為 : ' ,sum_len/len(data))
-
-
-
-
-
-
- 
+        if len(d) < 100:
+                new.append(d)
+print('一共有',len(new), '筆留言長度小於100')
